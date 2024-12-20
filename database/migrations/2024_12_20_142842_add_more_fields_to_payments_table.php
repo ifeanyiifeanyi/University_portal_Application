33ed2d65a0c1a9de1e89d6924bfa08aaca8178ad
Ifeanyi Nnaemego <josephifeanyid2@gmail.com>
2024-12-20 15:31:57 +0100

    added two columns to payments table, is_installment and total penalty amount

---
+++ database/migrations/2024_12_20_142842_add_more_fields_to_payments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->boolean('is_installment')->default(false);
+            $table->decimal('total_penalty_amount', 10, 2)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->dropColumn('is_installment');
+            $table->dropColumn('total_penalty_amount');
+        });
+    }
+};
